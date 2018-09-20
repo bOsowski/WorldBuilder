6290b7fa7dfee0da5d41af7a2bd5efde41e6deac
Bartosz Osowski <bOsowski04@gmail.com>
2018-09-20 14:56:55 +0100

    The sprites are now drawable. Layout looks good.

--- WorldBuilder/UserInterface.cpp
+++ WorldBuilder/UserInterface.cpp
@@ -13,7 +13,7 @@ UserInterface::UserInterface(){
 }
 
 void UserInterface::displayAvailableSprites(vector<sf::Sprite*> sprites){
-
+    
 }
 
 UserInterface& UserInterface::instance(){
@@ -23,33 +23,29 @@ UserInterface& UserInterface::instance(){
 
 
 void UserInterface::render(){
-    SFGUI sfgui;
+    
     mainWindow.resetGLStates();
+    sfg::SFGUI sfgui;
+    
     auto window = sfg::Window::Create();
-    window->SetTitle( "World Builder");
-    window->SetRequisition(Vector2f(mainWindow.getSize().x,mainWindow.getSize().y));
-    sf::View view( sf::Vector2f( 100.f, 100.f ), sf::Vector2f( mainWindow.getSize().x, mainWindow.getSize().y ) );
+    window->SetRequisition(Vector2f(mainWindow.getSize().x, mainWindow.getSize().y));
     
-    
-    // Create a box with 20 pixels spacing.
     auto box = sfg::Box::Create( sfg::Box::Orientation::HORIZONTAL, 5.f );
-    auto frame = sfg::Frame::Create( "Available Tiles" );
-    //auto separator = sfg::Separator::Create( sfg::Separator::Orientation::HORIZONTAL );
-    auto alignment = sfg::Alignment::Create();
-    auto alignment_box = sfg::Box::Create( sfg::Box::Orientation::VERTICAL );
-    auto canvas = sfg::Canvas::Create();
+    auto scrolled_window_box = sfg::Box::Create( sfg::Box::Orientation::VERTICAL);
+    
+    
+    auto scrolledWindow = sfg::ScrolledWindow::Create();
+    scrolledWindow->SetScrollbarPolicy( sfg::ScrolledWindow::VERTICAL_AUTOMATIC | sfg::ScrolledWindow::HORIZONTAL_NEVER );
+    scrolledWindow->AddWithViewport(scrolled_window_box);
+    
+    auto canvasWindow = sfg::Window::Create(sfg::Window::Style::BACKGROUND);
+    //canvasWindow->SetParent(box);
+    auto canvas = sfg::Canvas::Create(true);
+    canvasWindow->Add( canvas );
+    box->Pack(canvasWindow);
     auto table = sfg::Table::Create();
     
-    // Set scaling parameters.
-    // Scaling sets how much of the total space in the alignment
-    // the child gets to fill up. 1.0f for x and y would mean the
-    // child gets to use the whole space. Setting it to 0.0f for
-    // x and y makes the child get as little space as possible.
-    alignment->SetScale( sf::Vector2f( .0f, .0f ) );
     
-    alignment_box->Pack( alignment, false, false );
-    box->Pack( frame, false, false );
-    //box->Pack( separator, false, false );
     
     for(string* path: assetManager.imageAbsolutePaths){
         cout << "path = " + *path << endl;
@@ -57,24 +53,28 @@ void UserInterface::render(){
         auto image = sfg::Image::Create();
         if( imageSource.loadFromFile( *path ) ) {
             image->SetImage( imageSource );
+            auto button = sfg::Button::Create();
+            //currentlyPickedImage = (const sf::Image*)button->GetImage().get();
+            button->SetImage(image);
+            button->GetSignal( sfg::Widget::OnLeftClick ).Connect( [button, this] {
+                std::map<sfg::Button*, sf::Texture>::iterator it = buttonMap.find(button.get());
+                currentlyPickedImage = &it->second;
+            });
+            sf::Texture texture;
+            texture.loadFromImage(imageSource);
+            buttonMap.emplace(button.get(), texture);
+            scrolled_window_box->Pack(button);
         }
-        alignment_box->Pack( image);
     }
+    
 
+    scrolledWindow->SetRequisition(Vector2f(64, mainWindow.getSize().y/2));
+    box->Pack(scrolledWindow, false, false);
     
-    alignment->SetAlignment( sf::Vector2f( .8f, .8f ) );
-    frame->SetAlignment( sf::Vector2f( .8f, .5f ) );
     
-    frame->Add( alignment_box );
     window->Add( box );
-    
-    
-    
-    
-    
+    cout<<"Window size = ("<<mainWindow.getSize().x << ", " << mainWindow.getSize().y << ")\n";
 
-    //table->Attach(canvas, sf::Rect<sf::Uint32>( 0, 0, 1, 1 ));
-    //window->Add(canvas);
     while (mainWindow.isOpen())
     {
         // Process events
@@ -88,27 +88,29 @@ void UserInterface::render(){
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
                 mainWindow.close();
             }
+            if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && currentlyPickedImage != nullptr){
+                sf::Sprite* sprite = new Sprite();
+                sprite->setTexture(*currentlyPickedImage);
+                sprite->setOrigin(sprite->getTexture()->getSize().x/2, sprite->getTexture()->getSize().y/2);
+                sprite->setPosition((sf::Vector2f)sf::Mouse::getPosition(mainWindow));
+                world.push_back(sprite);
+            }
         }
-        // Update the GUI, note that you shouldn't normally
-        // pass 0 seconds to the update method.
+        
         window->Update( 0.f );
         mainWindow.clear();
-        // After drawing the rest of your game, you have to let the GUI
-        // render itself. If you don't do this you will never be able
-        // to see it ;)
-        sfgui.Display( mainWindow );
-        // Then the scrollable Canvas for SFML rendering.
         
-//        canvas->Bind();
-//        canvas->Clear( sf::Color( 0, 0, 0, 0 ) );
-//        canvas->SetView(view);
-//        for(int i = 0; i<assetManager.sprites.size(); i++){
-//            canvas->Draw(*assetManager.sprites[i]);
-//        }
-//        canvas->Display();
-//        canvas->Unbind();
-//
-        //displayAvailableSprites(assetManager.sprites);
+        canvas->Bind();
+        canvas->Clear( sf::Color( 0, 0, 0, 0 ), true );
+        
+        for(Sprite* sprite: world){
+            canvas->Draw(*sprite);
+        }
+        
+        canvas->Display();
+        canvas->Unbind();
+        sfgui.Display( mainWindow );
         mainWindow.display();
     }
 }
+
--- WorldBuilder/UserInterface.hpp
+++ WorldBuilder/UserInterface.hpp
@@ -11,18 +11,21 @@
 
 #include <stdio.h>
 #include "AssetManager.hpp"
-
-using namespace sfg;
+#include <map>
 
 class UserInterface{
 public:
     static UserInterface& instance();
     RenderWindow mainWindow{VideoMode(1600, 1200), "Asset Manager"};
+    map<sfg::Button*, sf::Texture> buttonMap;
+    const sf::Texture* currentlyPickedImage = nullptr;
+    
     void displayAvailableSprites(vector<Sprite*> sprites);
     void render();
 private:
     AssetManager assetManager{};
     UserInterface();
     const float padding = 20;
+    vector<sf::Sprite*> world;
 };
 #endif /* UserInterface_hpp */
